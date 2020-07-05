ccb019a703f495816cdd9c2152382ea4dee93b89
MarcosMartinez <31971392+MarkosAntonio@users.noreply.github.com>
2020-07-04 18:40:32 -0600

    base de datos
    nueva base

--- vuelos.sql
+++ vuelos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 03-07-2020 a las 23:23:43
+-- Tiempo de generación: 05-07-2020 a las 02:39:49
 -- Versión del servidor: 10.4.13-MariaDB
 -- Versión de PHP: 7.4.7
 
@@ -31,10 +31,19 @@ CREATE TABLE `avion` (
   `idavion` int(11) NOT NULL,
   `matricula` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `fabricante` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `modelo` int(100) NOT NULL,
-  `capcidad` int(11) NOT NULL,
-  `precio` decimal(10,2) NOT NULL
+  `modelo` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `capacidad` int(100) NOT NULL,
+  `precio` decimal(10,2) NOT NULL,
+  `estado` varchar(60) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `avion`
+--
+
+INSERT INTO `avion` (`idavion`, `matricula`, `fabricante`, `modelo`, `capacidad`, `precio`, `estado`) VALUES
+(3, 'MA', 'Rr', 'mm', 2, '50.00', 'ocupado'),
+(4, 'MV', 'R', '2', 300, '800.00', 'ocupado');
 
 -- --------------------------------------------------------
 
@@ -46,8 +55,17 @@ CREATE TABLE `vuelo` (
   `idvuelo` int(11) NOT NULL,
   `idavion` int(11) NOT NULL,
   `origen` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
-  `destino` varchar(200) COLLATE utf8_unicode_ci NOT NULL
+  `destino` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `estado` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `vuelo`
+--
+
+INSERT INTO `vuelo` (`idvuelo`, `idavion`, `origen`, `destino`, `estado`) VALUES
+(1, 4, 'El Salvado', 'España', 'ocupado'),
+(2, 4, 'El Salvador', 'Francia', 'libre');
 
 --
 -- Índices para tablas volcadas
@@ -74,13 +92,13 @@ ALTER TABLE `vuelo`
 -- AUTO_INCREMENT de la tabla `avion`
 --
 ALTER TABLE `avion`
-  MODIFY `idavion` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idavion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `vuelo`
 --
 ALTER TABLE `vuelo`
-  MODIFY `idvuelo` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idvuelo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Restricciones para tablas volcadas
